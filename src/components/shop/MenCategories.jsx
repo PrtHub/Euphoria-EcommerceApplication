4a2d7badf7837569cbd3a5b140e31ed3041bea95
Pritam <pri10145@gmail.com>
2023-07-25 09:38:32 +0530

    formatted

--- src/components/shop/MenCategories.jsx
+++ src/components/shop/MenCategories.jsx
@@ -6,12 +6,12 @@ const MenCategories = () => {
   return (
     <section className="w-full h-full flex flex-col items-start px-5 xl:px-10 py-5">
       <TitleCard title="Categories For Men" />
-     <section className="w-full h-full flex flex-wrap items-center justify-between overflow-hidden gap-10">
-      {ManCategories?.map((cat) => (
-        <main key={cat.id} className="w-[270px] h-[460px]">
-          <CatgoryCard img={cat.img}  title={cat.title}/>
-        </main>
-      ))}
+      <section className="w-full h-full flex flex-wrap items-center justify-between overflow-hidden gap-10">
+        {ManCategories?.map((cat) => (
+          <main key={cat.id} className="w-[270px] h-[460px]">
+            <CatgoryCard img={cat.img} title={cat.title} />
+          </main>
+        ))}
       </section>
     </section>
   );
