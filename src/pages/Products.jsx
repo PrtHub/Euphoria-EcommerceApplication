f1eb5a91a4af3ea1e6ac8351682961cad8ee4bd6
Pritam <pri10145@gmail.com>
2023-07-29 15:54:37 +0530

    formatted

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,7 @@ import { products } from "../data/data";
 import { FilterCard, ProductCard, SEO, TitleCard } from "../components";
 import { BsFilter } from "react-icons/bs";
 import { useState } from "react";
+import Select from "react-select";
 
 const Products = () => {
   const { category } = useParams();
@@ -13,6 +14,23 @@ const Products = () => {
     color: null,
   });
 
+  const [sortBy, setSortBy] = useState("lowToHigh");
+
+  const sortProductsByPrice = (products, sortBy) => {
+    const sortedProducts = [...products];
+    if (sortBy === "lowToHigh") {
+      sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "highToLow") {
+      sortedProducts.sort((a, b) => b.price - a.price);
+    }
+    return sortedProducts;
+  };
+
+  const sortOptions = [
+    { value: "lowToHigh", label: "Price: Low to High" },
+    { value: "highToLow", label: "Price: High to Low" },
+  ];
+
   const filteredProducts = products.filter(
     (product) => product.category === category
   );
@@ -22,7 +40,7 @@ const Products = () => {
   };
 
   let filteredProductsState = filteredProducts;
-  
+
   if (selectedFilters.tag) {
     filteredProductsState = filteredProductsState.filter(
       (product) => product.tag === selectedFilters.tag
@@ -47,30 +65,47 @@ const Products = () => {
       />
       <main className="w-full h-full">
         <section className="w-full h-full flex flex-col items-start justify-start px-5 xl:px-10 py-10 gap-10 ">
-          <header className="w-full h-full flex items-center ">
+          <header className="w-full h-full flex items-start lg:items-center ">
             <TitleCard title={`${category} Fashion Products`} />
-            <button
-              className="flex items-center gap-1 text-black-100 border-[1px] border-black-100 rounded px-2 py-1"
-              onClick={() => setOpenFilter(!openFilter)}
-            >
-              Filters
-              <BsFilter
-                className={`text-lg ${openFilter ? "rotate-180" : ""}`}
-                style={{ transition: "transform 0.5s" }}
+            <div className="flex flex-col lg:flex-row  items-center gap-4">
+              <Select
+                name="sort"
+                value={sortBy}
+                onChange={(selectedOption) => setSortBy(selectedOption.value)}
+                isClearable={true}
+                isSearchable={true}
+                options={sortOptions}
+                getOptionLabel={(option) => option.label}
+                getOptionValue={(option) => option.value}
+                placeholder="Sort by"
+                classNamePrefix="select"
+                className="w-[120px] sm:w-[200px] text-black outline-none"
               />
-            </button>
-          </header>
-          <section className="w-full h-full flex flex-wrap items-center justify-center lg:justify-start gap-10">
-            {filteredProductsState?.map((product) => (
-              <main key={product.id}>
-                <ProductCard
-                  img={product.img}
-                  title={product.title}
-                  price={product.price}
-                  brand={product.brand}
+              <button
+                className="w-[120px] sm:w-[200px] lg:w-28 flex justify-between items-center gap-4 text-[#808080] border-[1px] border-dark-white rounded px-3 py-[6px]"
+                onClick={() => setOpenFilter(!openFilter)}
+              >
+                Filters
+                <BsFilter
+                  className={`text-lg ${openFilter ? "rotate-180" : ""}`}
+                  style={{ transition: "transform 0.5s" }}
                 />
-              </main>
-            ))}
+              </button>
+            </div>
+          </header>
+          <section className="w-full h-full flex flex-wrap items-center justify-center md:justify-start gap-10">
+            {sortProductsByPrice(filteredProductsState, sortBy).map(
+              (product) => (
+                <main key={product.id}>
+                  <ProductCard
+                    img={product.img}
+                    title={product.title}
+                    price={product.price}
+                    brand={product.brand}
+                  />
+                </main>
+              )
+            )}
           </section>
         </section>
         {openFilter && (
