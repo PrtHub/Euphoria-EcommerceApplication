7a349895e120ae5efe19241411adfb9e9fc014cc
Pritam <pri10145@gmail.com>
2023-07-31 10:52:02 +0530

    sweater added

--- src/assets/images/Arrival/index.js
+++ src/assets/images/Arrival/index.js
@@ -10,8 +10,15 @@ import casual3 from './casual3.png'
 import casual4 from './casual4.png'
 import casual5 from './casual5.png'
 
-import track from "./Track.png";
 import sweater from "./sweater.png";
+import sweater1 from "./sweater1.png";
+import sweater2 from "./sweater2.png";
+import sweater3 from "./sweater3.png";
+import sweater4 from "./sweater4.png";
+import sweater5 from "./sweater5.png";
+
+
+import track from "./Track.png";
 import shorts from "./shorts.png";
 
 export {
@@ -27,8 +34,14 @@ export {
     casual3,
     casual4,
     casual5,
+    
+    sweater,
+    sweater1,
+    sweater2,
+    sweater3,
+    sweater4,
+    sweater5,
 
     shorts,
-    sweater,
     track
 }
