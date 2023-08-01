2f492d69028cd8be0c52e9e8cd87d453efccfbc2
Pritam <pri10145@gmail.com>
2023-08-01 15:39:30 +0530

    sskeleton build

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,13 @@
 import { Link, useParams } from "react-router-dom";
 import { products } from "../data/data";
 import { useState } from "react";
+import { Lazy } from "../components";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 
 const Product = () => {
   const { id } = useParams();
   const [goToCart, setGoToCart] = useState(false);
   const [quantity, setQuantity] = useState(1);
-
 
   const selectedProduct = products.find(
     (product) => product.id === parseInt(id)
@@ -16,70 +17,101 @@ const Product = () => {
     <main className="w-full h-full flex flex-col items-start px-5 xl:px-10 py-10 gap-10">
       <section className="w-full h-full flex flex-col lg:flex-row items-start  lg:items-center justify-start gap-10 lg:gap-20">
         <section className="w-full h-full sm:w-[410px] sm:h-[540px]">
-          <img
-            src={selectedProduct.img}
-            alt=""
-            className="w-full h-full  object-center"
+          <Lazy
+            img={selectedProduct.img}
+            className="w-full h-full sm:w-[410px] sm:h-[540px] object-center"
           />
         </section>
-        <section className="flex flex-col justify-start items-start gap-10">
-            <div className="flex flex-col items-start justify-start gap-4">
-              <h1 className="font-normal text-3xl md:text-4xl text-Primary">
+        <section className="flex flex-col justify-start items-start gap-6">
+          <header className="flex flex-col items-start justify-start gap-4">
+            <section className="flex flex-col items-start justify-start gap-1">
+              <h1 className="font-medium text-3xl md:text-4xl text-black-100 capitalize">
                 {selectedProduct.title}
               </h1>
-              <p className="font-normal text-Primary text-2xl">
+              <p className="capitalize text-light-gray text-xl font-medium">
+                {selectedProduct.brand}
+              </p>
+            </section>
+            <section className="">
+              <p className="font-normal text-black-100 text-2xl">
                 &#8377; {selectedProduct.price}
               </p>
-            </div>
-            <div className="max-w-5xl flex flex-col justify-start items-start gap-4">
-              <h3 className="font-normal text-xl text-Primary">
-                Product description
-              </h3>
-              <p className="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-                doloremque sapiente, tenetur nesciunt mollitia quaerat neque
-                dolores blanditiis, autem, id officiis dolore nihil dolor
-                assumenda dolorem. Quod dolorum excepturi quo.
+              <p className="text-dark-violet font-medium">
+                inclusive of all taxes
+              </p>
+            </section>
+          </header>
+          <article className="max-w-5xl flex flex-col justify-start items-start gap-2">
+            <h3 className="font-medium text-xl text-black-100">
+              Product description
+            </h3>
+            <p className="text-light-gray">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              doloremque sapiente, tenetur nesciunt mollitia quaerat neque
+              dolores blanditiis, autem, id officiis dolore nihil dolor
+              assumenda dolorem. Quod dolorum excepturi quo.
+            </p>
+          </article>
+          <section className="flex flex-col gap-2 items-start">
+            <h3 className="font-medium text-xl text-black-100">Select Size</h3>
+            <div className="flex items-center gap-4">
+              <p className="w-10 h-10 text-light-gray border-[1px] border-light-gray rounded-full p-4 flex items-center justify-center text-sm">
+                S
+              </p>
+              <p className="w-10 h-10 text-light-gray border-[1px] border-light-gray rounded-full p-4 flex items-center justify-center text-sm">
+                M
+              </p>
+              <p className="w-10 h-10 text-light-gray border-[1px] border-light-gray rounded-full p-4 flex items-center justify-center text-sm">
+                L
+              </p>
+              <p className="w-10 h-10 text-light-gray border-[1px] border-light-gray rounded-full p-4 flex items-center justify-center text-sm">
+                XL
+              </p>
+              <p className="w-10 h-10 text-light-gray border-[1px] border-light-gray rounded-full p-4 flex items-center justify-center text-sm">
+                2XL
               </p>
             </div>
-            <div className="flex flex-col items-start justify-start gap-4">
-              <h3 className="font-normal text-Primary text-xl">
-                Qunatity
-              </h3>
-              <div className="flex items-center justify-start gap-4 font-normal text-base text-Primary">
-                <span
-                  // className={`border-[1px] border-light-gray-100 px-4 py-2 cursor-pointer ${
-                  //   quantity <= 1 ? "opacity-50 pointer-events-none" : ""
-                  // }`}
-                  // onClick={() => handleQuantity("d")}
-                >
-                  -
-                </span>
-                <p>1</p>
-                <span
-                  className="border-[1px] border-light-gray-100 px-4 py-2 cursor-pointer"
-                  // onClick={() => handleQuantity("i")}
-                >
-                  +
-                </span>
-              </div>
-            </div>
-            <div className="flex items-center justify-start">
-              {goToCart ? (
-                <Link to="/cart" className="button">
-                  Go to cart
-                </Link>
-              ) : (
-                <button
-                  className="button"
-                  // onClick={handleAddToCart}
-                >
-                  Add to cart
-                </button>
-              )}
+          </section>
+          <section className="flex flex-col items-start justify-start gap-4">
+            <h3 className="font-medium text-xl text-black-100">Qunatity</h3>
+            <div className="flex items-center justify-start gap-4 font-normal text-base text-Primary">
+              <span
+              // className={`border-[1px] border-light-gray-100 px-4 py-2 cursor-pointer ${
+              //   quantity <= 1 ? "opacity-50 pointer-events-none" : ""
+              // }`}
+              // onClick={() => handleQuantity("d")}
+              >
+                -
+              </span>
+              <p>{quantity}</p>
+              <span
+                className="border-[1px] border-light-gray-100 px-4 py-2 cursor-pointer"
+                // onClick={() => handleQuantity("i")}
+              >
+                +
+              </span>
             </div>
           </section>
+          <div className="flex items-center justify-start">
+            {goToCart ? (
+              <Link
+                to="/cart"
+                className="bg-dark-violet text-white px-5 py-2 rounded"
+              >
+                Go to cart
+              </Link>
+            ) : (
+              <button
+                className="flex items-center  gap-2 bg-dark-violet text-white font-medium
+                 px-6 py-2 rounded"
+                // onClick={handleAddToCart}
+              >
+                <HiOutlineShoppingBag /> Add to cart
+              </button>
+            )}
+          </div>
         </section>
+      </section>
     </main>
   );
 };
