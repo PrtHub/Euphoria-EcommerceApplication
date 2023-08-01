aed8413d4eab720d3d424c11fbd6a487ee60b11c
Pritam <pri10145@gmail.com>
2023-08-01 12:23:17 +0530

    basic set up for product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,18 @@
+import { useParams } from "react-router-dom"
+import { products } from "../data/data"
 
 
 const Product = () => {
+  const { id } = useParams()
+
+  const selectedProduct = products.find(
+    (product) => product.id === parseInt(id)
+  );
+
   return (
-    <div>Product</div>
+    <main className="w-full h-full flex flex-col items-start px-5 xl:px-10 py-10 gap-10">
+     {selectedProduct.title}
+    </main> 
   )
 }
 
