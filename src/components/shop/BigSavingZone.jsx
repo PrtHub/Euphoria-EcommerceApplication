686822797a49c430b1cea1b51fc9e6e3e85dd8a8
Pritam <pri10145@gmail.com>
2023-08-01 08:30:50 +0530

    formatted

--- src/components/shop/BigSavingZone.jsx
+++ src/components/shop/BigSavingZone.jsx
@@ -15,8 +15,12 @@ const BigSavingZone = () => {
             <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl">
               Hawaiian <br /> Shirts
             </h1>
-            <p className="text-xs sm:text-[10px] md:text-xs">Dress up in summer vibe</p>
-            <h3 className="text-base sm:text-xs md:text-base font-semibold">UPTO 50% OFF</h3>
+            <p className="text-xs sm:text-[10px] md:text-xs">
+              Dress up in summer vibe
+            </p>
+            <h3 className="text-base sm:text-xs md:text-base font-semibold">
+              UPTO 50% OFF
+            </h3>
 
             <button className="bg-transparent border-[1px] border-white text-white font-medium rounded py-1 px-3 lg:px-4 text-base sm:text-xs md:text-base">
               Shop Now
@@ -33,8 +37,12 @@ const BigSavingZone = () => {
               Printed
               <br /> T-Shirt{" "}
             </h1>
-            <p className="text-xs sm:text-[10px] md:text-xs">New Designs Every Week</p>
-            <h3 className="text-base sm:text-xs md:text-base font-semibold">UPTO 40% OFF</h3>
+            <p className="text-xs sm:text-[10px] md:text-xs">
+              New Designs Every Week
+            </p>
+            <h3 className="text-base sm:text-xs md:text-base font-semibold">
+              UPTO 40% OFF
+            </h3>
             <button className="bg-transparent border-[1px] border-white text-white font-medium rounded py-1 px-3 lg:px-4 text-base sm:text-xs md:text-base">
               Shop Now
             </button>
@@ -49,7 +57,9 @@ const BigSavingZone = () => {
             <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl text-end md:text-start">
               Cargo <br /> Joggers
             </h1>
-            <p className="text-xs sm:text-[10px] md:text-xs">Move with style & comfort</p>
+            <p className="text-xs sm:text-[10px] md:text-xs">
+              Move with style & comfort
+            </p>
             <h3 className="text-base sm:text-xs md:text-base font-semibold">
               UPTO 50% OFF
             </h3>
@@ -63,8 +73,8 @@ const BigSavingZone = () => {
             src={urban}
             className="w-full h-full object-cover object-center"
           />
-           <article className="absolute top-10 sm:top-8 lg:top-20 right-10 xl:right-20 flex flex-col sm:items-end md:items-start gap-4 sm:gap-2 md:gap-4 text-black-100">
-          <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl">
+          <article className="absolute top-10 sm:top-8 lg:top-20 right-10 xl:right-20 flex flex-col sm:items-end md:items-start gap-4 sm:gap-2 md:gap-4 text-black-100">
+            <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl">
               Urban <br /> Shirts
             </h1>
             <p className="text-xs sm:text-[10px] md:text-xs">Live in comfort</p>
@@ -82,10 +92,12 @@ const BigSavingZone = () => {
             className="w-full h-full object-cover object-center"
           />
           <article className="absolute top-10 sm:top-8 lg:top-20 right-10 xl:right-20 flex flex-col sm:items-end md:items-start gap-4 sm:gap-2 md:gap-4 text-black-100">
-          <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl text-end md:text-start">
+            <h1 className="font-semibold text-2xl sm:text-base md:text-2xl lg:text-4xl text-end md:text-start">
               Oversized <br /> T-Shirts
             </h1>
-            <p className="text-xs sm:text-[10px] md:text-xs">Street Style Icon</p>
+            <p className="text-xs sm:text-[10px] md:text-xs">
+              Street Style Icon
+            </p>
             <h3 className="text-base sm:text-xs md:text-base font-semibold">
               UPTO 60% OFF
             </h3>
