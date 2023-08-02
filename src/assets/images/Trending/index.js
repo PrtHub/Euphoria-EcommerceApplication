aeb6c4b13e525c9c76e5b120e430ed810bb36db3
Pritam <pri10145@gmail.com>
2023-08-02 15:36:17 +0530

    Images adding for Tranding section

---
+++ src/assets/images/Trending/index.js
@@ -0,0 +1,8 @@
+import T1 from "./T1.png";
+import T6 from "./T6.png";
+import T2 from "./T2.png";
+import T3 from "./T3.png";
+import T4 from "./T4.png";
+import T5 from "./T5.png";
+
+export { T1, T2, T3, T4, T5, T6 };
