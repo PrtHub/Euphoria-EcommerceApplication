bf64b5caa2f0213d048d983c5377670eb62feaf4
Pritam <pri10145@gmail.com>
2023-08-03 20:34:09 +0530

    seater images added

---
+++ src/assets/images/women/index.js
@@ -0,0 +1,19 @@
+import ws1 from './ws1.png'
+import ws2 from './ws2.png'
+import ws3 from './ws3.png'
+import ws4 from './ws4.png'
+import ws5 from './ws5.png'
+import ws6 from './ws6.png'
+import ws7 from './ws7.png'
+import ws8 from './ws8.png'
+
+export {
+    ws1,
+    ws2,
+    ws3,
+    ws4,
+    ws5,
+    ws6,
+    ws7,
+    ws8
+}
