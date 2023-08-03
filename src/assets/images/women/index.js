ebc5eec8f11cf7568a8aa25ef98ebfc41d2676cb
Pritam <pri10145@gmail.com>
2023-08-03 20:55:09 +0530

    more images added

--- src/assets/images/women/index.js
+++ src/assets/images/women/index.js
@@ -6,6 +6,8 @@ import ws5 from './ws5.png'
 import ws6 from './ws6.png'
 import ws7 from './ws7.png'
 import ws8 from './ws8.png'
+import ws9 from './ws9.png'
+import ws10 from './ws10.png'
 
 export {
     ws1,
@@ -15,5 +17,7 @@ export {
     ws5,
     ws6,
     ws7,
-    ws8
+    ws8,
+    ws9,
+    ws10
 }
