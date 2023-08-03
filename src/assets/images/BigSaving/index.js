3f55c1679dd9bbaa3e258c039a8d3372fbe444a3
Pritam <pri10145@gmail.com>
2023-08-03 10:29:42 +0530

    add images

---
+++ src/assets/images/BigSaving/index.js
@@ -0,0 +1,14 @@
+import hawai1 from './hawai1.png'
+import hawai2 from './hawai2.png'
+import hawai3 from './hawai3.png'
+import hawai4 from './hawai4.png'
+import hawai5 from './hawai5.png'
+
+
+export {
+    hawai1,
+    hawai2,
+    hawai4,
+    hawai5,
+    hawai3
+}
