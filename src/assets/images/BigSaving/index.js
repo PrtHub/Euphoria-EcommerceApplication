b2d1246fd18ef3e6bf646177c0b6a129a79d0017
Pritam <pri10145@gmail.com>
2023-08-03 11:07:24 +0530

    cargo joggers completed

--- src/assets/images/BigSaving/index.js
+++ src/assets/images/BigSaving/index.js
@@ -4,11 +4,23 @@ import hawai3 from './hawai3.png'
 import hawai4 from './hawai4.png'
 import hawai5 from './hawai5.png'
 
+import cargo1 from './cargo1.png'
+import cargo2 from './cargo2.png'
+import cargo3 from './cargo3.png'
+import cargo4 from './cargo4.png'
+import cargo5 from './cargo5.png'
+
 
 export {
     hawai1,
     hawai2,
     hawai4,
     hawai5,
-    hawai3
+    hawai3,
+
+    cargo1,
+    cargo2,
+    cargo3,
+    cargo4,
+    cargo5
 }
