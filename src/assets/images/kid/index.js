5db9e7415005c029364b430a98b36e560ad2f99e
Pritam <pri10145@gmail.com>
2023-08-04 09:45:45 +0530

    add images

---
+++ src/assets/images/kid/index.js
@@ -0,0 +1,13 @@
+import cs1 from './cs1.png';
+import cs2 from './cs2.png';
+import cs3 from './cs3.png';
+import cs4 from './cs4.png';
+import cs5 from './cs5.png';
+
+export {
+    cs1,
+    cs2,
+    cs3,
+    cs4,
+    cs5
+}
