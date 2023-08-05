187ec0588818f47ce9907dd9c6298fe537432f2a
Pritam <pri10145@gmail.com>
2023-08-05 13:14:57 +0530

    jackets added

--- src/assets/images/men/index.js
+++ src/assets/images/men/index.js
@@ -1,66 +1,79 @@
-import jog1 from './jog1.png'
-import jog2 from './jog2.png'
-import jog3 from './jog3.png'
-import jog4 from './jog4.png'
+import jog1 from "./jog1.png";
+import jog2 from "./jog2.png";
+import jog3 from "./jog3.png";
+import jog4 from "./jog4.png";
 
-import trouser1 from './trouser1.png'
-import trouser2 from './trouser2.png'
-import trouser3 from './trouser3.png'
-import trouser4 from './trouser4.png'
-import trouser5 from './trouser5.png'
-import trouser6 from './trouser6.png'
-import trouser7 from './trouser7.png'
-import trouser8 from './trouser8.png'
-import trouser9 from './trouser9.png'
-import trouser10 from './trouser10.png'
+import trouser1 from "./trouser1.png";
+import trouser2 from "./trouser2.png";
+import trouser3 from "./trouser3.png";
+import trouser4 from "./trouser4.png";
+import trouser5 from "./trouser5.png";
+import trouser6 from "./trouser6.png";
+import trouser7 from "./trouser7.png";
+import trouser8 from "./trouser8.png";
+import trouser9 from "./trouser9.png";
+import trouser10 from "./trouser10.png";
 
-import ts1 from './ts1.png'
-import ts2 from './ts2.png'
-import ts3 from './ts3.png'
-import ts4 from './ts4.png'
-import ts5 from './ts5.png'
-import ts6 from './ts6.png'
-import ts7 from './ts7.png'
+import ts1 from "./ts1.png";
+import ts2 from "./ts2.png";
+import ts3 from "./ts3.png";
+import ts4 from "./ts4.png";
+import ts5 from "./ts5.png";
+import ts6 from "./ts6.png";
+import ts7 from "./ts7.png";
 
-import hs1 from './hs1.png'
-import hs2 from './hs2.png'
-import hs3 from './hs3.png'
-import hs4 from './hs4.png'
-import hs5 from './hs5.png'
-import hs6 from './hs6.png'
+import hs1 from "./hs1.png";
+import hs2 from "./hs2.png";
+import hs3 from "./hs3.png";
+import hs4 from "./hs4.png";
+import hs5 from "./hs5.png";
+import hs6 from "./hs6.png";
 
-
+import jk1 from "./jk1.png";
+import jk2 from "./jk2.png";
+import jk3 from "./jk3.png";
+import jk4 from "./jk4.png";
+import jk5 from "./jk5.png";
+import jk6 from "./jk6.png";
+import jk7 from "./jk7.png";
+import jk8 from "./jk8.png";
+import jk9 from "./jk9.png";
 
 export {
-    jog1,
-    jog2,
-    jog4,
-    jog3,
-
-    trouser1,
-    trouser2,
-    trouser3,
-    trouser4,
-    trouser5,
-    trouser6,
-    trouser7,
-    trouser8,
-    trouser9,
-    trouser10,
-
-    ts1,
-    ts2,
-    ts3,
-    ts4,
-    ts5,
-    ts6,
-    ts7,
-
-
-    hs1,
-    hs2,
-    hs3,
-    hs4,
-    hs5,
-    hs6
-}
+  jog1,
+  jog2,
+  jog4,
+  jog3,
+  trouser1,
+  trouser2,
+  trouser3,
+  trouser4,
+  trouser5,
+  trouser6,
+  trouser7,
+  trouser8,
+  trouser9,
+  trouser10,
+  ts1,
+  ts2,
+  ts3,
+  ts4,
+  ts5,
+  ts6,
+  ts7,
+  hs1,
+  hs2,
+  hs3,
+  hs4,
+  hs5,
+  hs6,
+  jk1,
+  jk2,
+  jk3,
+  jk4,
+  jk5,
+  jk6,
+  jk7,
+  jk8,
+  jk9,
+};
