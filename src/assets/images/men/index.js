7576d87f4404ac866b0c08d6781170e59a383c12
Pritam <pri10145@gmail.com>
2023-08-05 09:46:55 +0530

    images added

--- src/assets/images/men/index.js
+++ src/assets/images/men/index.js
@@ -8,6 +8,11 @@ import trouser2 from './trouser2.png'
 import trouser3 from './trouser3.png'
 import trouser4 from './trouser4.png'
 import trouser5 from './trouser5.png'
+import trouser6 from './trouser6.png'
+import trouser7 from './trouser7.png'
+import trouser8 from './trouser8.png'
+import trouser9 from './trouser9.png'
+import trouser10 from './trouser10.png'
 
 
 export {
@@ -20,5 +25,10 @@ export {
     trouser2,
     trouser3,
     trouser4,
-    trouser5
+    trouser5,
+    trouser6,
+    trouser7,
+    trouser8,
+    trouser9,
+    trouser10
 }
