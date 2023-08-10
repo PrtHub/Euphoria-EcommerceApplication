1f0e3cc8a2409fff3dcfaa5c34f5d00b6ba70a0e
Pritam <pri10145@gmail.com>
2023-08-10 19:23:34 +0530

    add bugfender

--- src/utils/Bugfender.js
+++ src/utils/Bugfender.js
@@ -3,3 +3,15 @@ import { Bugfender } from '@bugfender/sdk';
 Bugfender.init({
 	appKey: '01QCPGoYXVR75KKwX2uAO86kAOAYBY1I',
 });
+
+const logEvent = (tag, message) => {
+	Bugfender.d(tag, message);
+  };
+
+  const logError = (errorTag, error) => {
+	Bugfender.e(errorTag, error);
+  };
+  
+  export default { logEvent, logError };
+
+  
