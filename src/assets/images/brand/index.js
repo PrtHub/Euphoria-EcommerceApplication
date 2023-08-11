8dac6ff4ca3131c398ab28234993ba1bec70d3bb
Pritam <pri10145@gmail.com>
2023-08-11 10:18:48 +0530

    puma completd

--- src/assets/images/brand/index.js
+++ src/assets/images/brand/index.js
@@ -11,8 +11,11 @@ import nj2 from './nj2.png';
 import njo from './njo.png';
 import njo1 from './njo1.png';
 
-import puma1 from './puma1.png';
-
+import puma1 from './puma1.png'
+import pj from './pj.png';
+import pj2 from './pj2.png';
+import ps from './ps.png';
+import ps2 from './ps2.png';
 
 import levi1 from './levi1.png';
 
@@ -23,6 +26,10 @@ export  {
     ust,
 
     puma1,
+    ps,
+    ps2,
+    pj,
+    pj2,
 
     levi1,
     ns,
