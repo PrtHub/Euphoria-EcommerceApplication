7e777b72b798da9335058299e0de91d21bb312d8
Pritam <pri10145@gmail.com>
2023-08-11 09:38:12 +0530

    us images

--- src/assets/images/brand/index.js
+++ src/assets/images/brand/index.js
@@ -1,4 +1,7 @@
 import us1 from './us1.png';
+import uss from './uss.png';
+import ust from './ust.png';
+import usj from './usj.png';
 
 import ns from './ns.png';
 import nt1 from './nt1.png';
@@ -15,6 +18,10 @@ import levi1 from './levi1.png';
 
 export  {
     us1,
+    usj,
+    uss,
+    ust,
+
     puma1,
 
     levi1,
